8038f44a0d3e4de3354c6382a12361e111b6fe1e
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2025-02-10 16:16:27 +0530

    commit.java

--- Karan.java
+++ Karan.java
@@ -1,6 +1,13 @@
 public class Karan{
 public static void main(String[] args){
 System.out.println("hello world");
+System.out.println("please write your name");
+System.out.println("my name  is karan chaudhary");
+System.out.println("please enter your age ");
+System.out.println("my age is 21 ");
+System.out.println("please enter your course");
+
+
 
 }
 }
---
+++ vscode1/test.java
@@ -0,0 +1,10 @@
+package vscode1;
+
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+    }
+
+}
