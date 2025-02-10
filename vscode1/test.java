807110262944e3a3bae11041aa4bcc8e1606ffe8
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2025-02-11 00:59:56 +0530

    commit java

--- vscode1/test.java
+++ vscode1/test.java
@@ -3,7 +3,21 @@ package vscode1;
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        System.out.println("welcome to mini calculator");
+
+        int a = 90;
+        int b = 40;
+        int sum = a + b;
+        int sub = a - b;
+        int multi = a * b;
+        float div = a / b;
+        int mod = a % b;
+
+        System.out.println("print sum number "+"= " +  a + " + " + b +  " = "  + sum);
+        System.out.println("print sub number "+"= " + a + " - " + b + " = " + sub);
+        System.out.println("print multi number "+"= " + a + " * " + b + " = " + multi);
+        System.out.println("print div number "+"= " + a + " / " + b + " = " + div);
+        System.out.println("print mod number "+"= " + a + " % " + b + " = " + mod);
 
     }
 
