55badb1b33ee19e1221cf16a6597538cd16dd88a
Karan Kumar <149156353+mrkarankumar@users.noreply.github.com>
2025-02-10 13:21:37 +0530

    commit java

---
+++ Karan.java
@@ -0,0 +1,6 @@
+public class Karan{
+public static void main(String[] args){
+System.out.println("hello world");
+
+}
+}
---
+++ abc.java
@@ -0,0 +1,16 @@
+  class Test{
+	  
+	// main method jdk 1.5 var-rage args
+	
+	public static void main(String ...a){
+		System.out.println("Hello java world!");
+		int x = 20;
+		int y = 40;
+		int z;
+		z= x+y;
+		 
+		
+		System.out.println("sum ="+z);
+		
+	}
+}
